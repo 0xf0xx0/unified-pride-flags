647694570f3b4a9bf79e2d5a6856cd56c16dc3f8
Gingka/Ginger Pepper <33764485+ExperiBass@users.noreply.github.com>
2024-05-08 23:54:30 -0400

    add .prettierrc, give variables in build script more descriptive names

--- contrib/buildlist.js
+++ contrib/buildlist.js
@@ -1,5 +1,5 @@
-function sortObj(obj) {
-    let entries = Object.entries(obj).sort((a,b) => {
+function sortObjByKey(obj) {
+    let entries = Object.entries(obj).sort((a, b) => {
         if (a[0] < b[0]) {
             return -1
         }
@@ -7,29 +7,33 @@ function sortObj(obj) {
     })
     return Object.fromEntries(entries)
 }
+
 function cleanFlags(flagObj) {
-    flagObj = sortObj(flagObj)
-    
+    flagObj = sortObjByKey(flagObj)
+
     // format hex colors
-    let formattedEntries = []
-    for (let [name, obj] of Object.entries(flagObj)) {
-        obj.stripes = obj.stripes.map(v => v.toLowerCase())
-        formattedEntries.push([name, obj])
+    let cleanedFlags = {}
+    for (let [name, flag] of Object.entries(flagObj)) {
+        /// good heavens walter, i cannot decide between upper and lower
+        flag.stripes = flag.stripes.map((hex) => hex.toLowerCase())
+        cleanedFlags[name] = flag
     }
-    return Object.fromEntries(formattedEntries)
+    return cleanedFlags
 }
+
 function readJSONC(path) {
     path = join(__dirname, path)
     const jsonc = readFileSync(path, 'utf8')
     // TODO: this probably needs to be better
-    const json = JSON.parse(jsonc.replace(/ {0,}\/\/.+/gm, '').replace(/\/\*.+?\*\//igm))
+    const json = JSON.parse(jsonc.replace(/ {0,}\/\/.+/gm, '').replace(/\/\*.+?\*\//gim))
     return json
 }
-const {readFileSync, writeFileSync} = require('node:fs')
+
+const { readFileSync, writeFileSync } = require('node:fs')
 const YAML = require('yaml')
-const {join} = require('node:path')
+const { join } = require('node:path')
 const flags = readJSONC('../unified-flags.jsonc')
 
 const cleaned = cleanFlags(flags)
-writeFileSync(join(__dirname, '../unified-flags.json'), JSON.stringify(cleaned, null, 4))
-writeFileSync(join(__dirname, '../unified-flags.yaml'), YAML.stringify(cleaned, null, 4))
+writeFileSync(join(__dirname, '../unified-flags.json'), JSON.stringify(cleaned))
+writeFileSync(join(__dirname, '../unified-flags.yaml'), YAML.stringify(cleaned))
